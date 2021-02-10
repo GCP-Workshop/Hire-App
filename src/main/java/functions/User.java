5c4814834d7981285652c4cdc032211431a4611b
Abilash <abilashr@thoughtworks.com>
2021-02-10 18:07:49 +0530

    Removed phone from processing

--- src/main/java/functions/User.java
+++ src/main/java/functions/User.java
@@ -4,13 +4,11 @@ public class User {
     public String name;
     public Integer age;
     public String place;
-    public String phone;
 
-    public User(String name, Integer age, String place, String phone) {
+    public User(String name, Integer age, String place) {
         this.name = name;
         this.age = age;
         this.place = place;
-        this.phone = phone;
     }
 
 }
